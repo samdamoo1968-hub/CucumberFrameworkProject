e7e1564d607063b486fc5a6e6c52da26a30183bb
samdamoo1968-hub <samdamoo1968@gmail.com>
2026-05-15 13:07:47 +0530

    Refactor driver initialization with ChromeOptions

--- src/test/java/demowebshop_tricentis/Base/DriverManager.java
+++ src/test/java/demowebshop_tricentis/Base/DriverManager.java
@@ -3,18 +3,29 @@ package demowebshop_tricentis.Base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 public class DriverManager {
 
-    protected WebDriver driver;
+    public WebDriver initialize_driver() {
 
-    public WebDriver initialize_driver(){
         WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Required for GitHub Actions
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         return driver;
     }
 }
