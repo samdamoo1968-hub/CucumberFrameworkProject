ee939d7fcdd7174dfae0da5d9ef1ec812b4c46e6
samdamoo1968-hub <samdamoo1968@gmail.com>
2026-05-15 13:07:29 +0530

    Refactor TestBase for improved WebDriver management

--- src/test/java/demowebshop_tricentis/Base/TestBase.java
+++ src/test/java/demowebshop_tricentis/Base/TestBase.java
@@ -1,77 +1,53 @@
 package demowebshop_tricentis.Base;
 
 import demowebshop_tricentis.Constants.Global_Constants;
-import demowebshop_tricentis.Pages.AccountPage;
-import demowebshop_tricentis.Pages.HomePage;
-import demowebshop_tricentis.Pages.LoginPage;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
 import org.openqa.selenium.WebDriver;
 
 public class TestBase {
 
     Global_Constants global_constants = new Global_Constants();
 
+    // Thread-safe WebDriver
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
-    // Set driver in ThreadLocal
+    // Set driver
     public void setDriver(WebDriver driverInstance) {
         driver.set(driverInstance);
     }
 
-    // Get driver from ThreadLocal
+    // Get driver
     public static WebDriver getdriver() {
         return driver.get();
     }
 
-    // Start driver and return it
+    // Initialize driver
     public WebDriver start_driver() {
-        setDriver(new DriverManager().initialize_driver());
+
+        DriverManager driverManager = new DriverManager();
+
+        setDriver(driverManager.initialize_driver());
+
+        getdriver().manage().window().maximize();
+
         return getdriver();
     }
 
-    // Launch application URL
+    // Open application URL
     public void OpenUrl() {
+
         getdriver().get(global_constants.URL);
     }
 
-    // Close / quit browser safely
+    // Quit browser safely
     public void CloseUrl() {
+
         WebDriver driverInstance = getdriver();
+
         if (driverInstance != null) {
+
             driverInstance.quit();
-            driver.remove(); // ✅ cleanup ThreadLocal to avoid memory leaks
+
+            driver.remove(); // cleanup ThreadLocal
         }
     }
 }
-
-
-//    Global_Constants global_constants = new Global_Constants();
-//
-//    public static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-//
-//
-//    public void setdriver(WebDriver driver) {
-//        this.driver.set(driver);
-//    }
-//
-//    public static WebDriver getdriver() {
-//        return driver.get();
-//    }
-//
-//
-//    public void start_driver() {
-//
-//        setdriver(new DriverManager().initialize_driver());
-//    }
-//
-//    public void OpenUrl() {
-//        getdriver().get(global_constants.URL);
-//    }
-//
-//    public void CloseUrl() {
-//        getdriver().close();
-//    }
-
-
